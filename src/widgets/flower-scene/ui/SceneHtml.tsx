14dc1d1716d29aa7ec6f4ebc8f686cfdfdda7b38
Han <rhaudgks04@gmail.com>
2026-09-01 21:09:09 +0900

    💄 style: import linter 적용하여 import 순서 통일화

--- src/widgets/flower-scene/ui/SceneHtml.tsx
+++ src/widgets/flower-scene/ui/SceneHtml.tsx
@@ -1,6 +1,7 @@
+import { Html } from "@react-three/drei";
+
 import { MenuButton } from "@/src/features/open-menu/ul/MenuButton";
 import { WaterButton } from "@/src/features/water-flower/ui/WaterButton";
-import { Html } from "@react-three/drei";
 
 export function SceneHtml() {
   return (
