6078fd5dd6e0beb4f52f5879f34945ac7be21c8b
Han <124041261+Gomyeunghan@users.noreply.github.com>
2026-09-03 17:55:59 +0900

    ♻️ refactor: Navigation컴포넌트 개발로 미사용되는 menuButton import삭제

--- src/widgets/flower-scene/ui/SceneHtml.tsx
+++ src/widgets/flower-scene/ui/SceneHtml.tsx
@@ -1,6 +1,5 @@
 import { Html } from "@react-three/drei";
 
-import { MenuButton } from "@/src/features/open-menu/ul/MenuButton";
 import { WaterButton } from "@/src/features/water-flower/ui/WaterButton";
 
 export function SceneHtml() {
